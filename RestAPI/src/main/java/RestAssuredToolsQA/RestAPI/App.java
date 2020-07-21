95e28632690f0b8462ad60d222c68da3ca0039f6
Amnsingh <amnsingh36@gmail.com>
2020-07-21 17:26:43 +0530

    init commit

---
+++ RestAPI/src/main/java/RestAssuredToolsQA/RestAPI/App.java
@@ -0,0 +1,13 @@
+package RestAssuredToolsQA.RestAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
